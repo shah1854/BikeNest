9123bf3904096403f82e0c68da4cc30794072fea
mihirshah <shahmihirl07@gmail.com>
2023-10-03 14:37:34 -0500

    Linked the "Continue as Guest" button on the login page to LoginActivity, allowing the user to go to the MainActivity without logging in.

--- app/src/main/java/com/example/bicycleparkingproject/LoginActivity.java
+++ app/src/main/java/com/example/bicycleparkingproject/LoginActivity.java
@@ -1,13 +1,51 @@
 package com.example.bicycleparkingproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.AuthResult;
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.FirebaseUser;
+
+
 public class LoginActivity extends AppCompatActivity {
+    TextInputEditText etEmailLogin, etPasswordLogin;
+    Button btnLogin, btnGoToRegister, btnGoToRegister2;
+    //FirebaseAuth mAuth;
+    //FirebaseAuth mAuth;
+    ProgressBar progressBar;
+    //TextView tvIntro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        etEmailLogin = findViewById(R.id.etEmailLogin);
+        etPasswordLogin = findViewById(R.id.etPasswordLogin);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnGoToRegister = findViewById(R.id.btnGoToRegister);
+        btnGoToRegister2 = findViewById(R.id.btnGoToRegister2);
+
+        btnGoToRegister2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
 }
