dcfb80781a444a0cae3e28a7ac3256fb739026ba
mihirshah <shahmihirl07@gmail.com>
2023-10-23 16:12:48 -0500

    Added first name and last name to the FireStore based off of the authenticated user's ID

--- app/src/main/java/com/example/bicycleparkingproject/RegisterActivity.java
+++ app/src/main/java/com/example/bicycleparkingproject/RegisterActivity.java
@@ -9,11 +9,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import android.text.TextUtils;
@@ -23,13 +26,16 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegisterActivity extends AppCompatActivity {
     TextInputEditText editTextEmail, editTextPassword, editTextFirstName, editTextLastName, editTextPasswordConfirm;
     Button btnRegister, btnLogin;
 //    ProgressBar progressBar;
     FirebaseAuth mAuth;
     FirebaseFirestore db;
-
+    CollectionReference reference;
 
     @Override
     public void onStart() {
@@ -54,6 +60,8 @@ public class RegisterActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnGoToLogin);
         btnRegister = findViewById(R.id.btnRegister);
         db = FirebaseFirestore.getInstance();
+        reference = db.collection("UserInformation");
+
 //        progressBar = findViewById(R.id.progressBar);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -70,11 +78,11 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View v) {
 //                progressBar.setVisibility(View.VISIBLE);
                 String email, password, confirmPassword, firstName, lastName;
-                email = String.valueOf(editTextEmail.getText());
-                password = String.valueOf(editTextPassword.getText());
-                confirmPassword = String.valueOf(editTextPasswordConfirm.getText());
-                firstName = String.valueOf(editTextFirstName.getText());
-                lastName = String.valueOf(editTextLastName.getText());
+                email = String.valueOf(editTextEmail.getText()).trim();
+                password = String.valueOf(editTextPassword.getText()).trim();
+                confirmPassword = String.valueOf(editTextPasswordConfirm.getText()).trim();
+                firstName = String.valueOf(editTextFirstName.getText()).trim();
+                lastName = String.valueOf(editTextLastName.getText()).trim();
 
                 if (TextUtils.isEmpty(email)) {
                     Toast.makeText(RegisterActivity.this, "Enter Email", Toast.LENGTH_SHORT).show();
@@ -111,6 +119,8 @@ public class RegisterActivity extends AppCompatActivity {
                                     Log.d(TAG, "createUserWithEmail:success");
                                     FirebaseUser user = mAuth.getCurrentUser();
 //                                    progressBar.setVisibility(View.GONE);
+                                    String uid = user.getUid();
+                                    addData(uid, firstName, lastName, reference);
                                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                                     startActivity(intent);
                                     finish();
@@ -122,6 +132,31 @@ public class RegisterActivity extends AppCompatActivity {
                             }
                         });
             }
+
         });
+
+    }
+    private void addData(String uid, String firstName, String lastName, CollectionReference reference) {
+        Log.d(TAG, "reached addData");
+//        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        Map<String, String> info = new HashMap<>();
+        info.put("uid", uid);
+        info.put("First Name", firstName);
+        info.put("Last Name", lastName);
+
+        reference.document(uid)
+                .set(info)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
     }
 }
