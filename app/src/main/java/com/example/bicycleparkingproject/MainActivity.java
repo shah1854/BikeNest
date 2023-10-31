28ddb5a678f2fc9de99411276f038cd2716f0fde
Anand Krishnan <akrishx@gmail.com>
2023-10-30 20:20:08 -0500

    Removed extra layout features from activity_main

--- app/src/main/java/com/example/bicycleparkingproject/MainActivity.java
+++ app/src/main/java/com/example/bicycleparkingproject/MainActivity.java
@@ -48,15 +48,11 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private static final int PERMISSIONS_FINE_LOCATION = 99;
-    //reference to UI elements
-    TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updates, tv_address;
-    Button btn_newWayPoint, btn_showMap;
-
-    Switch sw_locationupdates, sw_gps;
+    //reference to UI element
 
     //variable to remember if we are tracking location or no
     boolean updateOn = false;
-
+    Button btn_newWayPoint, btn_showMap;
     Location currentLocation;
 
     //location request is a config file
@@ -68,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     private static final String TAG = "MainActivity";
-
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     private final CollectionReference bikeRackRef = db.collection("BikeRacks");
     private DocumentReference rackRef = db.document("BikeRacks/First Bike Rack");
@@ -83,39 +78,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv_lat = findViewById(R.id.tv_lat);
-        tv_lon = findViewById(R.id.tv_lon);
-        tv_altitude = findViewById(R.id.tv_altitude);
-        tv_accuracy = findViewById(R.id.tv_accuracy);
-        tv_speed = findViewById(R.id.tv_speed);
-        tv_sensor = findViewById(R.id.tv_sensor);
-        tv_updates = findViewById(R.id.tv_updates);
-        tv_address = findViewById(R.id.tv_address);
-        sw_gps = findViewById(R.id.sw_gps);
-        sw_locationupdates = findViewById(R.id.sw_locationsupdates);
         btn_newWayPoint = findViewById(R.id.btn_newWayPoint);
-        btn_showMap = findViewById(R.id.button3);
-
-        // set all properties of location request
-        locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 30000)
-                .setWaitForAccurateLocation(false)
-                .setMinUpdateIntervalMillis(2000)
-                .setMaxUpdateDelayMillis(100)
-                .build();
-
-
-        locationCallBack = new LocationCallback() {
-
-            @Override
-            public void onLocationResult(@NonNull LocationResult locationResult) {
-                super.onLocationResult(locationResult);
-
-                //save the location
-
-                updateUIValues(locationResult.getLastLocation());
-            }
-        };
-
+        btn_showMap = findViewById(R.id.btn_showMap);
         btn_newWayPoint.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -157,117 +121,7 @@ public class MainActivity extends AppCompatActivity {
                 double longitude = coordinates.getLongitude();
             }
         }
-
-        sw_locationupdates.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (sw_locationupdates.isChecked()) {
-                    //turn on location tracking
-                    startLocationUpdates();
-                } else {
-                    // turn off tracking
-                    stopLocationUpdates();
-                }
-            }
-        });
-
-        updateGPS();
     } // end of onCreate method
-
-    private void stopLocationUpdates() {
-        tv_updates.setText("Location is NOT being tracked");
-        tv_lat.setText("Not tracking location");
-        tv_lon.setText("Not tracking location");
-        tv_speed.setText("Not tracking location");
-        tv_address.setText("Not tracking location");
-        tv_accuracy.setText("Not tracking location");
-        tv_altitude.setText("Not tracking location");
-        tv_sensor.setText("Not tracking location");
-
-        fusedLocationProviderClient.removeLocationUpdates(locationCallBack);
-    }
-
-    private void startLocationUpdates() {
-
-        tv_updates.setText("Location is being tracked");
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallBack, null);
-        updateGPS();
-    }
-
-    private void updateGPS() {
-        //get permission from user to track GPS
-        //get current location from the fused client
-        //Update the UI
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            //user provided the permission
-            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    // we got permissions. Put values of location
-                    //updateUIValues(location);
-                    currentLocation = location;
-
-                }
-            });
-        }
-        else {
-            //permission not granted yet
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_FINE_LOCATION);
-            }
-        }
-    }
-
-    private void updateUIValues(Location location) {
-
-        //update text view objects with new location
-        if (tv_lat != null && tv_lon != null && tv_accuracy != null) {
-            tv_lat.setText(String.valueOf(location.getLatitude()));
-            tv_lon.setText(String.valueOf(location.getLongitude()));
-            tv_accuracy.setText(String.valueOf(location.getAccuracy()));
-        }
-
-
-        if (location.hasAltitude()) {
-            tv_altitude.setText(String.valueOf(location.hasAltitude()));
-        }
-        else {
-            tv_altitude.setText("Not available");
-        }
-
-        if (location.hasSpeed()) {
-            tv_speed.setText(String.valueOf(location.getAltitude()));
-
-        }
-        else {
-            tv_speed.setText("Not available");
-        }
-
-        Geocoder geocoder = new Geocoder(MainActivity.this);
-
-        try {
-            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-            tv_address.setText(addresses.get(0).getAddressLine(0));
-
-        }
-        catch (Exception e) {
-            tv_address.setText("Unable to get street address");
-        }
-    }
-
 
     private Coordinates parseCoordinatesFromString(String location) {
         String cleanedLocation = location.replaceAll("[^0-9.,-]", "");
@@ -281,11 +135,8 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
-
-
         return null; // Handle invalid format
     }
-
 
     @Override
     protected void onStart() {
@@ -308,28 +159,12 @@ public class MainActivity extends AppCompatActivity {
         //toastFirstRack();
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        switch (requestCode) {
-            case PERMISSIONS_FINE_LOCATION:
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                updateGPS();
-            }
-            else {
-                Toast.makeText(this, "This app requires permission to be granted", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        }
-    }
-
     private void toastFirstRack() {
         HANDLER.postDelayed(new Runnable() {
             @Override
             public void run() {
                 // Do something after 5s = 5000ms
-                Toast.makeText(MainActivity.this, "Bike rack size: " + bikeRacks.size(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "First bike rack: " + bikeRacks.get(0).getId(), Toast.LENGTH_SHORT).show();
             }
         }, DELAY_TIME);
     }
